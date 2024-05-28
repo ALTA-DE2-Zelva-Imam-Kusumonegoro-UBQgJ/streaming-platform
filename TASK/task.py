8cfcda4ae12a888e33b5422caa5d0e64d32b239f
zelvaiz <zelvaimam15@gmail.com>
2024-05-28 23:27:20 +0700

    upload tugsa

--- TASK/task.py
+++ TASK/task.py
@@ -1,9 +1,6 @@
-import json
 import time
 from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
-from confluent_kafka.avro.serializer import SerializerError
-
 
 schema_str = """
 {
@@ -20,29 +17,19 @@ schema_str = """
 
 schema = avro.loads(schema_str)
 
-
 avro_producer_config = {
-    'bootstrap.servers': 'localhost:19092',
-    'on_delivery': lambda err, msg: print(f'Delivery report: {err}' if err else f'Message produced: {msg.value()}'),
+    'bootstrap.servers': 'localhost:18080',
     'schema.registry.url': 'http://localhost:18081'
 }
 
 producer = AvroProducer(avro_producer_config, default_value_schema=schema)
 
+def send_stock_data(symbol, price):
+    timestamp = int(time.time())
+    stock_data = {"symbol": symbol, "price": price, "timestamp": timestamp}
+    producer.produce(topic='stock_avro_topic', value=stock_data)
+    producer.flush()
+    print(f"Produced stock data: {stock_data}")
 
-def send_stock_data(symbol, price, timestamp):
-    stock_data = {
-        "symbol": symbol,
-        "price": price,
-        "timestamp": timestamp
-    }
-    try:
-        producer.produce(topic='stock_avro_topic', value=stock_data)
-        producer.flush()
-        print(f"Produced stock data: {stock_data}")
-    except SerializerError as e:
-        print(f"Message serialization failed: {e}")
-
-# Example usage: publish some stock data
-send_stock_data("AAPL", 150.0, int(time.time()))
-send_stock_data("GOOGL", 2800.0, int(time.time()))
+send_stock_data("AAPL", 150.0)
+send_stock_data("GOOGL", 2800.0)
